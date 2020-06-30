8ccc84aa81d05e59263c41b5d96dbef9fac406af
Ruth B Densamo <rbd218@lehigh.edu>
2020-06-30 12:47:11 -0400

    added knujon data and scoring

---
+++ classes/KnujOn.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class KnujOn:
+    def __init__(self, path):
+        self.__knuj_domains_dict = dict()
+
+        # If we would rather read it from a flat file:
+        # r = open(path, 'r').read()
+        # soup = BeautifulSoup(r, 'html.parser')
+
+        # Getting html from website:
+        r = requests.get('http://www.knujon.com/registrars/')
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        soup = soup.ol
+        results = soup.find_all('li')
+
+        for reg in results:
+            entry = reg.text
+            # TODO: Need to store the registrar key in a way that we can do a lookup of the registrars (all lowercase?)
+            # TODO: Manually look through like registrar prices - compare against elastic search
+            # TODO: substring ? much harder to do samething as registrarnprices because there is more entries
+            # TODO: similarity score : based on how many consecutive characters are the same & thresholding - expensive
+            reg_name = entry.split(':')[0]
+            reg_score = entry.split(':')[1]
+            self.__knuj_domains_dict[reg_name] = reg_score
+            # print(reg_name)
+            # print(reg_score)
+
+    def score(self, domain):
+        entry = self.__knuj_domains_dict.get(domain.registrar, None)
+        domain.set_subscore("knujOn",
+                            {"score": entry,
+                             "registrar": domain.registrar})
+
+
+
+knu = KnujOn("../datasets/KnujOn.html")
+# reg = Registrarprices("../TLD_PRICING/TLD_PRICES_AVGBYREG.csv")
---
+++ classes/Registrarprices.py
@@ -0,0 +1,55 @@
+from urllib import parse
+from csv import DictReader
+
+
+class Registrarprices:
+    """
+
+    """
+
+    def __init__(self, path):
+        self.avg_prices = dict()
+        with open(path, "r") as f:
+
+            reader = DictReader(f)
+
+            for row in reader:
+                registrar = row["Registrar"]
+                if row["Average_Price"] in [None, ""]:
+                    break
+
+                # TODO: Double check if this needs to be optimized
+                pt_entry = row
+
+                entry_price = row["Average_Price"]
+                self.avg_prices[registrar] = float(entry_price)
+
+        sorted_price = list(self.avg_prices.values())
+        sorted_price.sort(reverse=True)
+        max_price = float(sorted_price[0])
+
+        for key in self.avg_prices.keys():
+            scaled_prices = dict(ave_price=float(self.avg_prices[key]),
+                                 scaled_price=float(self.avg_prices[key]) / max_price)
+            # Overwriting with new scaled and original price
+            self.avg_prices[key] = scaled_prices
+
+    def score(self, domain):
+        # Check if registrar is in our list
+        real_reg = None
+
+        for registrar in self.avg_prices.keys():
+            if registrar.lower() in domain.registrar.lower():
+                real_reg = registrar
+                break
+
+        print(self.avg_prices[real_reg])
+        if real_reg is None:
+            domain.set_subscore("prices", {"score": 0.6, "note": "Registrar price info not found"})
+            return
+        domain.set_subscore("prices", {"score": self.avg_prices[real_reg]["scaled_prices"],
+                                       "note": "scoring with scaled registrar prices"})
+
+# TODO: Coverage using counter to see how many domains scored
+# TODO: We substring the registrar on the domain looking for key from average_prices dict
+# reg = Registrarprices("../TLD_PRICING/TLD_PRICES_AVGBYREG.csv")
