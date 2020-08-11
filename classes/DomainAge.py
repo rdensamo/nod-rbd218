3c3afbac89e058d91eb63b8d36c18ec75e6fd63b
Forest B. Crowley <fbc216@lehigh.edu>
2020-08-11 11:52:57 -0400

    fix domain age interval lookup

--- classes/DomainAge.py
+++ classes/DomainAge.py
@@ -4,21 +4,19 @@ from bisect import bisect_left
 class DomainAge:
     # Approximated from the charts on page 4 and 5 of
     # https://www.domaintools.com/content/The_DmainTools_Report_Distribution_Malicious_Domain.pdf
-    intervals_mo = range(0, 84, 3)
+    intervals_mo = range(0, 81, 3)
     intervals_mo_lookup = (1.45, 1.80, 3.60, 3.85, 2.10, 1.65, 1.10, 0.90, 0.40, 0.42,
                            0.49, 0.39, 0.30, 0.28, 0.31, 0.39, 0.29, 0.14, 0.20, 0.19,
                            0.18, 0.17, 0.15, 0.15, 0.14, 0.20, 0.19, 0.09)
 
     @staticmethod
     def score(domain):
-        if domain.age < 34560:
+        if domain.age < 2.074e+6:
             # First 24 days of life are most suspicious
             domain.set_subscore("age", {"score": 4.0})
         else:
             # Otherwise defer to the domaintools research
-            # FIXME: This is totally wrong, should look up the offset and get its value, instead it's
-            # looking up the value based on age in months, which will always be larger than all values in
-            # the table.
-            months = domain.age / 43800
-            score = DomainAge.intervals_mo_lookup[bisect_left(DomainAge.intervals_mo, months)]
+            months = domain.age / 2.628e+6
+            offset = bisect_left(DomainAge.intervals_mo, months)
+            score = DomainAge.intervals_mo_lookup[offset]
             domain.set_subscore("age", {"score": score})
