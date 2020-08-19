fbf114fc449f33bfa196dbe0e956ca1475ca4504
Forest B. Crowley <fbc216@lehigh.edu>
2020-08-19 13:51:32 -0400

    add script that writes out full elasticsearch domain tracker dataset as json

---
+++ scripts/get_elk_domains.py
@@ -0,0 +1,43 @@
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
+import json
+from pprint import pprint
+from classes import Domain
+
+ES_SOCKET = "127.0.0.1:9200"
+
+# Create elasticsearch object,
+es = Elasticsearch([ES_SOCKET])
+
+# Build query using lucene query string
+query = Q("query_string",
+          query="brotype:dns-tracker AND _exists_:age")
+
+# Only get domains from the 2020.04.29 BRO index
+search = Search(using=es, index="bro-*")
+
+# Execute the query
+res = search.query(query)
+
+# Get results from ES one at a time, parse into Domain objects, add objects
+# to domains.
+
+documents = list()
+
+for hit in res.scan():
+    current_domain = Domain.Domain(getattr(hit, "query", None),
+                                   getattr(hit, 'registrar', None),
+                                   getattr(hit, "age", None))
+    documents.append(current_domain.__dict__)
+
+with open("hits.json", "w") as f:
+    f.write(json.dumps(documents))
+
+
+# to load the file from disk
+"""
+data = None
+with open("hits.json", "w") as f:
+    data = json.loads(f.read())
+
+"""
